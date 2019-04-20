eebf9ccc9e1479f874a134ff755c0e4e45374ec8
Terminator <alexey.orlov2016@yandex.ru>
2019-04-20 13:28:08 +0400

    Add files via upload

---
+++ footer.py
@@ -0,0 +1,37 @@
+footer = """<div class="container-fluid text-light bg-dark h-100">
+  <footer class="container pt-4 my-md-5 pt-md-5 text-light bg-dark container-main">
+    <div class="row">
+      <div class="col-12 col-md">
+        <img class="mb-2" src="img/logo.png" width="50" height="30" alt="">
+        <small class="d-block mb-3 text-muted">&copy; 2019</small>
+      </div>
+      <!-- <div class="col-6 col-md">
+        <h5>Features</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="text-muted" href="#">Team feature</a></li>
+          <li><a class="text-muted" href="#">Stuff for developers</a></li>
+          <li><a class="text-muted" href="#">Another one</a></li>
+          <li><a class="text-muted" href="#">Last time</a></li>
+        </ul>
+      </div> -->
+      <!-- <div class="col-6 col-md">
+        <h5>Resources</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="text-muted" href="#">Resource</a></li>
+          <li><a class="text-muted" href="#">Resource name</a></li>
+          <li><a class="text-muted" href="#">Another resource</a></li>
+          <li><a class="text-muted" href="#">Final resource</a></li>
+        </ul>
+      </div> -->
+      <div class="col-6 col-md">
+        <h5>About</h5>
+        <ul class="list-unstyled text-small">
+          <li><a class="text-muted" href="https://github.com/orgs/mad-penguins/people">Team</a></li>
+          <li><a class="text-muted" href="#">Locations</a></li>
+          <li><a class="text-muted" href="#">Privacy</a></li>
+          <!-- <li><a class="text-muted" href="#">Terms</a></li> -->
+        </ul>
+      </div>
+    </div>
+  </footer>
+  </div>"""
