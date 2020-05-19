eabf28c729937bd929cad89681a9179cf4f17d8e
fresh-ter <alexey.orlov2016@yandex.ru>
2020-05-19 20:50:58 +0400

    Added commands 'update var' and 'test var'

--- freezer.py
+++ freezer.py
@@ -1,5 +1,5 @@
 import configparser
-from os import mkdir
+from os import mkdir, walk, path
 
 class Freezer(object):
     """docstring for Freezer"""
@@ -8,6 +8,32 @@ class Freezer(object):
         self.args = args
         self.workingDirectory = workingDirectory
 
+    def getListFromConfig(self, section, key):
+        config = configparser.ConfigParser()
+        config.read(self.args[1])
+
+        a = config.get(section, key)
+        a = a.strip('][').split(', ')
+        for x in range(len(a)):
+            a[x] = a[x].strip("'")
+
+        if (len(a) == 1) and (a[0] == ''):
+            a = list()
+        return a
+
+    def writeConfig(self, section, option, value, nameConfig=None):
+        if nameConfig is None:
+            nameConfig = self.args[1]
+
+        config = configparser.ConfigParser()
+        config.read(nameConfig)
+
+        if not config.has_section(section):
+            config.add_section(section)
+        config[section][option] = str(value)
+
+        with open(nameConfig, "w") as config_file:
+            config.write(config_file)
 
     def startNewProject(self, nameNewProject = None):
         if nameNewProject is None:
@@ -24,6 +50,7 @@ class Freezer(object):
         config.add_section("Snowflakes *.html")
         config.add_section("Snowflakes *.py")
         config.add_section("Snowflakes *.vfreezer")
+        config['Snowflakes *.vfreezer']['path'] = str(list())
 
         with open(nameNewProject + '.freezer', "w") as config_file:
             config.write(config_file)
@@ -32,7 +59,60 @@ class Freezer(object):
         mkdir("snow")
         mkdir("src")
         mkdir("static")
-        mkdir("vars")
+        mkdir("var")
+
+    def getFileAddressList(self, rootAddress = None):
+        if rootAddress is None:
+            rootAddress = "."
+
+        fileAddressList = list()
+
+        for root, dirs, files in walk(rootAddress):
+            for name in files:
+                fileAddressList.append(path.join(root, name))
+        return fileAddressList
+
+    def updateVar(self):
+        fileList = self.getFileAddressList('var')
+        self.writeConfig("Snowflakes *.vfreezer", "path", fileList)
+        #fileListFromConfig = self.getListFromConfig("Snowflakes *.vfreezer", "path")
+        #print('fileList: ', fileList, len(fileList))
+        #print('fileListFromConfig: ', fileListFromConfig, len(fileListFromConfig))
+        #print(path.isfile(fileListFromConfig[0]))
+        # with open(fileListFromConfig[0], "r") as c:
+        #     print(c.read())
+
+    def testVar(self):
+        fileListFromConfig = self.getListFromConfig("Snowflakes *.vfreezer", "path")
+        
+        answer = ''
+
+        if len(fileListFromConfig) > 0:
+            answer = 'FileName\t\t\tAvailable\n'
+            answer += '--------\t\t\t---------\n'
+            for x in fileListFromConfig:
+                answer += x + '\t\t' + str(path.isfile(x)) + '\n'
+        else:
+            answer = 'Files are not registered in the config.'
+        return answer
+
+    def update(self, whatToUpdate = None):
+        if whatToUpdate is None:
+            whatToUpdate = self.args[3]
+
+        if whatToUpdate == 'var':
+            self.updateVar()
+
+    def test(self, whatToTest = None):
+        if whatToTest is None:
+            whatToTest = self.args[3]
+
+        answer = ''
+
+        if whatToTest == 'var':
+            answer = self.testVar()
+        return answer
+        
         
             
 
--- run.py
+++ run.py
@@ -1,44 +1,17 @@
-import configparser
 from os import getcwd
 from sys import argv
 from freezer import Freezer
-
-def createConfig(path):
-    """
-    Create a config file
-    """
-    config = configparser.ConfigParser()
-    config.add_section("Settings")
-    config.set("Settings", "font", "Courier")
-    config.set("Settings", "font_size", "10")
-    config.set("Settings", "font_style", "Normal")
-    config.set("Settings", "font_info",
-                "You are using %(font)s at %(font_size)s pt")
-
-    test = ["test1","test2","test3","test4"]
-
-    config.add_section("Path to templates files")
-    config['Path to templates files']["test1"] = str(test)
-    #config['Path']["test2"]
-    #config['Path']["test3"]
-    #print(config)
-    #print(type(config))
-    #config.set("Path","test", None)
-    #config.set("Path","test1", None)
-    
-    with open(path, "w") as config_file:
-        config.write(config_file)
-
 
 def main():
 	f = Freezer(argv, getcwd())
 
 	if argv[1] == 'start':
 		f.startNewProject()
-
-
+	else:
+		if argv[2] == 'update':
+			f.update()
+		elif argv[2] == 'test':
+			print(f.test())
 
 if __name__ == "__main__":
-    # path = "project11.freezer"
-    # createConfig(path)
     main()
